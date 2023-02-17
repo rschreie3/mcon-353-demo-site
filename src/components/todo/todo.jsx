99e9f7db581ed27901bc38aacf7357c2b66d1a83
rschreie3 <97309686+rschreie3@users.noreply.github.com>
2023-02-17 10:22:08 -0500

    fixed bugs in todo

--- src/components/todo/todo.jsx
+++ src/components/todo/todo.jsx
@@ -6,28 +6,45 @@ export const Todo = () => {
   const [input, setInput] = useState("");
 
   const [todos, setTodos] = useState([
-    "finish homework",
-    "eat dinner",
-    "sleep",
+    {
+      title: "finish homework",
+      isComplete: false,
+    },
+    {
+      title: "sleep",
+      isComplete: false,
+    },
   ]);
+
+  const toggleChecked = (todo) => {
+    const newTodos = [...todos];
+    const updatedTodo = newTodos.find((x) => x.title === todo.title);
+    updatedTodo.isComplete = !todo.isComplete;
+    setTodos(newTodos);
+  };
 
   const onInput = (event) => {
     setInput(event.target.value);
   };
 
   const addTodo = () => {
-    setTodos([...todos, input]);
+    setTodos([...todos, {title: input, isComplete: false}]);
+    setInput("");
   };
 
   return (
     <div>
       <h1>Todos</h1>
-      <input onInput={onInput} />
+      <input onInput={onInput} value={input} />
       <button onClick={addTodo}>Add</button>
-      {todos.map((todo) => (
-        <p>
-          <input type="checkbox" />
-          {todo}
+      {todos.map((todo, index) => (
+        <p key={index}>
+          <input
+            type="checkbox"
+            checked={todo.isComplete}
+            onChange={()=> toggleChecked(todo)}
+          />
+          {todo.title}
         </p>
       ))}
     </div>
