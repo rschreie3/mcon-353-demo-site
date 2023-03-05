eecda19337e90154a879d70e6f58124c4bc1b8a9
rschreie3 <97309686+rschreie3@users.noreply.github.com>
2023-03-05 17:38:34 -0500

    changed to global state so todos stay even with navigation

--- src/state/todo/todo-reducer.js
+++ src/state/todo/todo-reducer.js
@@ -1,7 +1,20 @@
-const todoReducer = (state, action) => {
+import { cloneDeep } from "lodash";
+
+export const todoReducer = (state, action) => {
   switch (action.type) {
     case "ADD": {
       return { todos: [...state.todos, action.todo] };
+    }
+    case "DELETE": {
+      return { todos: [] };
+    }
+    case "TOGGLE": {
+      let newTodos = cloneDeep(state.todos);
+      const updatedTodo = newTodos.find((x) => x.title === action.todo.title);
+      updatedTodo.isComplete = !updatedTodo.isComplete;
+      return {
+        todos: newTodos,
+      };
     }
   }
 };
@@ -9,4 +22,9 @@ const todoReducer = (state, action) => {
 const addAction = {
   type: "ADD",
   todo: { title: "whatever", isComplete: false },
+};
+
+const toggleAction = {
+  type: "TOGGLE",
+  todo: {},
 };
